6fb8475c0ebce13577f17ac911d473d4c9923636
Md Altaf Hoshain <altaf@squaregroup.com>
2024-07-17 17:21:54 +0600

    Dashboard is updated

--- lib/features/dashboard/dashboard_page.dart
+++ lib/features/dashboard/dashboard_page.dart
@@ -1,5 +1,7 @@
+import 'package:edcr_sup_flutter/common/utils/my_text_style.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../../common/utils/my_colors.dart';
 import '../../common/widgets/custom_app_bar.dart';
 
 //
@@ -19,9 +21,63 @@ class DashboardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: CustomAppBar(title: "Dashboard"),
-      body: Center(child: Text("Dashboard")),
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: const BoxDecoration(
+            color: MyColors.backgroundLight, // Your background color
+          ),
+          child: const Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 10, right: 10, bottom: 50, top: 50),
+                child: Row(
+                  children: [
+                    Text(
+                      "Sample App",
+                      style: MyTextStyle.titleStyle,
+                    ),
+                    Spacer(),
+                    Text(
+                      "17 July, 2024",
+                      style: MyTextStyle.titleStyle,
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                elevation: 10,
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("data"),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("0",style: TextStyle(fontSize: 20),),
+                            Text("Title",style: MyTextStyle.textStyle,),
+                          ],
+                        ),
+                      ),
+                      Spacer(),
+                      Text("data2 ",style: MyTextStyle.textStyle,),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
